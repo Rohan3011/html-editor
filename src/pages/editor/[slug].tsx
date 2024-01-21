c80478f5679dbd95a4d2bc0da8f2f0f775dfa0c0
rohanop <rohanopdev@gmail.com>
2024-01-21 21:22:14 +0530

    Added author field

--- src/pages/editor/[slug].tsx
+++ src/pages/editor/[slug].tsx
@@ -15,8 +15,10 @@ const myFont = localFont({
 export default function EditorPage() {
   const router = useRouter();
   const editorRef = useRef<any>(null);
+  const [author, setAuthor] = useState("");
   const [htmlPreview, setHtmlPreview] = useState<any>("");
   const [saveTimeout, setSaveTimeout] = useState<any>(null);
+  const [showModal, setShowModal] = useState(false);
 
   const [isSaving, setIsSaving] = useState(false);
 
@@ -24,9 +26,15 @@ export default function EditorPage() {
     editorRef.current = editor;
   }
 
+  const handleClose = () => setShowModal(false);
+
   const mutation = useMutation({
     mutationFn: (post: Partial<Post>) => {
       return axios.post("/api/editor", post);
+    },
+    onSuccess: () => {
+      setAuthor("");
+      setShowModal(true);
     },
   });
 
@@ -47,8 +55,11 @@ export default function EditorPage() {
     setSaveTimeout(newTimeout);
   };
 
-  const handleSubmit = async () => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (author.length < 2) return;
     mutation.mutate({
+      author: author,
       filename: String(router.query.slug),
       body: htmlPreview,
     });
@@ -57,12 +68,49 @@ export default function EditorPage() {
   return (
     <>
       <div className="p-4 flex">
-        <button
-          onClick={handleSubmit}
-          className={`flex justify-center items-center px-6 py-3 bg-neutral-950 rounded-2xl overflow-hidden  shadow-[2px_2px_0px_1px_#353333] border active:scale-95 transition-all duration-150 uppercase  ${myFont.className}`}
+        <form
+          onSubmit={handleSubmit}
+          className={`flex gap-10 ${myFont.className}`}
         >
-          Submit
-        </button>
+          <div className="flex w-full max-w-xl bg-neutral-50 rounded-2xl overflow-hidden shadow-[4px_4px_0px_1px_#353333] focus-within:shadow-[4px_4px_0px_1px_#313131] transition-all">
+            <input
+              type="text"
+              value={author}
+              onChange={(e) => setAuthor(e.target.value)}
+              placeholder="Your Name"
+              className="flex-1  h-full bg-transparent text-black outline-none px-8 "
+            />
+          </div>
+          <button
+            type="submit"
+            className={`flex justify-center items-center px-6 py-3 bg-neutral-950 rounded-2xl  shadow-[2px_2px_0px_1px_#353333] border active:scale-95 transition-all duration-150 uppercase  `}
+          >
+            {mutation.isPending && (
+              <svg
+                className="animate-spin -ml-1 mr-3 h-5 w-5  "
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+            )}
+            Submit
+          </button>
+        </form>
+
         {isSaving && (
           <p className="ml-auto  px-6 py-3 flex justify-center items-center  bg-neutral-700 rounded-2xl overflow-hidden">
             <svg
@@ -77,7 +125,7 @@ export default function EditorPage() {
                 cy="12"
                 r="10"
                 stroke="currentColor"
-                stroke-width="4"
+                strokeWidth="4"
               ></circle>
               <path
                 className="opacity-75"
@@ -108,6 +156,57 @@ export default function EditorPage() {
           ></iframe>
         </div>
       </div>
+
+      <SuccessModal
+        filename={router.query.slug as string}
+        id={mutation.data?.data?._id ?? ""}
+        open={showModal}
+        handleClose={handleClose}
+      />
     </>
+  );
+}
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+type Props = {
+  filename: string;
+  id: string;
+  open: boolean;
+  handleClose: () => void;
+};
+
+export function SuccessModal({ filename, id, open, handleClose }: Props) {
+  const router = useRouter();
+
+  const handler = () => {
+    router.push("/post/" + id);
+  };
+  return (
+    <AlertDialog open={open} onOpenChange={handleClose}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            {" "}
+            {filename} is deployed successfully 🎉
+          </AlertDialogTitle>
+          <AlertDialogDescription>wanna see it?</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Close</AlertDialogCancel>
+          <AlertDialogAction onClick={handler}>Go To Live</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
